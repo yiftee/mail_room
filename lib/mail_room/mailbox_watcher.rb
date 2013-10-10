5c0d389be352f930d7eb76c3161710ac7115477e
Jon Kepecs <jkepecs@ip-10-170-99-148.us-west-1.compute.internal>
2013-10-10 22:59:33 +0000

    multiple fixes to add a watchdog on the imap thread, handle various race conditions and exceptions; logging

--- lib/mail_room/mailbox_watcher.rb
+++ lib/mail_room/mailbox_watcher.rb
@@ -5,18 +5,24 @@ module MailRoom
 
     def initialize(mailbox)
       @mailbox = mailbox
-
+      @done_lock = Mutex.new
       @running = false
       @logged_in = false
       @idling = false
     end
 
     def imap
+      wlog("S1", "")
       @imap ||= Net::IMAP.new('imap.gmail.com', :port => 993, :ssl => true)
+      wlog("S2", "")
+      @imap
     end
 
     def handler
+      wlog("S3", @mailbox)
       @handler ||= MailboxHandler.new(@mailbox, imap)
+      wlog("S4", @mailbox)
+      @handler
     end
 
     def running?
@@ -37,26 +43,111 @@ module MailRoom
     end
 
     def log_in
+      if @mailbox != nil then
+        wlog("S5", @mailbox.email + "---" + @mailbox.password)
+      end
       imap.login(@mailbox.email, @mailbox.password)
+      if @mailbox != nil then
+        wlog("S6", @mailbox.email + "---" + @mailbox.password)
+      end
       @logged_in = true
     end
 
     def set_mailbox
-      imap.select(@mailbox.name) if logged_in?
+      if @mailbox != nil then
+        wlog("S7", @mailbox.name)
+      end
+      res = imap.select(@mailbox.name) if logged_in?
+      if @mailbox != nil then
+        wlog("S8", @mailbox.name)
+      end
+      res
+    end
+
+    # log stuff for debugging.
+    def wlog(state, msg="")
+      if false then
+        `echo #{state} #{msg} >> /tmp/watcher`
+      end
+    end
+
+    def reset_imap(where, msg)
+      wlog("RESET", msg)
+      if imap.disconnected? then
+        wlog("DISCONNECTED", msg)
+      else
+        wlog("BROKEN", msg)
+        # We don't want to try idle_done since if we're not in idle state, this will
+        # throw an exception.  There aren't a lot of primitives available; disconnect is the only
+        # other thing to try.
+        begin
+          imap.disconnect
+        rescue Exception => e
+          wlog("PANIC", e)
+          raise "WATCHER"
+        end
+      end
+      begin
+        @imap = nil
+        @logged_in = false
+        @handler = nil
+        @idling = false  # prevent watchdogs
+        setup
+      rescue Exception => e
+        wlog("FATAL", e)
+        raise "WATCHER" # kills thread
+      end
     end
 
     def idle
       return unless logged_in?
 
-      @idling = true
+      begin
+        clean = true
 
-      imap.idle do |response|
-        if response.respond_to?(:name) && response.name == 'EXISTS'
-          imap.idle_done
+        begin
+          wlog("c", "")
+          # We can't really protect idle from a disconnect caused by an exception in the main watchdog process.
+          # Presumably, if this happens, we'll get an exception if the idle is attempted prior to the idle command.
+          # The mutex only serves to protect against an idle_done at the wrong time, and since idle_done itself doesn't block
+          # we can use mutex to protect it.
+          @idling = true
+
+          while (@imap == nil) || imap.disconnected? do
+            wlog("waiting for imap", "")
+            sleep(1)
+          end
+
+          imap.idle do |response|
+            wlog("y", "")
+            if response.respond_to?(:name) && response.name == 'EXISTS'
+              # yield a response -- there's mail to read.  Still in idle state, so set to done
+              # so we can cleanly re-enter idle (it's an error to idle while in idle state).
+              wlog("E", "")
+              @done_lock.lock
+              if @idling then
+                imap.idle_done
+                @idling = false
+              end
+              @done_lock.unlock
+            else
+              # We may get here if the watchdog forces an idle_done.
+              # In this case, we don't want another idle_done (which will cause an error).
+              # We simply return, and process_messages will find nothing to do; we'll end up back in idle.
+              wlog("X", "")
+            end  # imap.idle
+          end
+          wlog("c1", "")
+        rescue Exception => e
+          wlog("E2", e)
+          clean = false
+          reset_imap("idle", e)
         end
-      end
+
+      end while !clean
 
       @idling = false
+
     end
 
     def process_mailbox
@@ -70,18 +161,58 @@ module MailRoom
       idling_thread.join
     end
 
+    # http://ruby-doc.org/stdlib-1.9.3/libdoc/net/imap/rdoc/Net/IMAP.html
     def run
       setup
 
       @running = true
 
       self.idling_thread = Thread.start do
-        while(running?) do
+        while (running?) do
           # block until we stop idling
-          idle
-          # when new messages are ready
-          process_mailbox
+          wlog("i", "")
+          idle     # when idle returns, new messages are ready or idle got somehow interrupted.
+          wlog("p", "")
+          begin
+            if (@imap != nil) && !imap.disconnected? then
+              wlog("pr", "")
+
+              process_mailbox
+
+              wlog("pr_done", "")
+            end
+          rescue Exception => e
+            reset_imap("thread", e)
+          end
         end
+      end
+
+      # Main program; watches over the thread.
+      # Note that idle_done is the only available primitive to try to reset things.
+      # disconnect in the middle of an idle appears fatal but might be worth another look.
+      # kill must use -9 if we're in the middle of an idle to nuke the whole process.
+      # kill -INT only works if disconnected.
+      loop do
+        sleep(60 * 10)  # imap times out after 29 min so 10 should be conservative
+        wlog("a", "")
+        if @idling then  ### this might need to be in a critical section.
+          begin
+            # force keepalive; thread will then idle again.
+            @done_lock.lock
+            if @idling then
+              wlog("POKE_TRY", "")
+              if (@imap != nil) && !imap.disconnected? then
+                wlog("POKED", "")
+                imap.idle_done
+              end
+              @idling = false
+            end
+            @done_lock.unlock
+          rescue Exception => e
+            reset_imap("watchdog", e)
+          end
+        end
+        wlog("b", "")
       end
     end
 
@@ -89,5 +220,7 @@ module MailRoom
       @running = false
       stop_idling
     end
-  end
-end
+
+  end # class
+
+end # module

c3d300e0d78b2113f341f8ce5a166ff9f210794e
Jon Kepecs <jon@yiftee.com>
2014-04-07 04:53:26 +0000

    url-encode the POST params to please rack

--- lib/mail_room/delivery/postback.rb
+++ lib/mail_room/delivery/postback.rb
@@ -1,4 +1,5 @@
 require 'faraday'
+require 'uri'
 
 module MailRoom
   module Delivery
@@ -11,12 +12,20 @@ module MailRoom
         connection = Faraday.new
         connection.token_auth @mailbox.delivery_token
 
+        # ERROR ArgumentError: invalid %-encoding ("f T-KT-JX"  can be seen as a rack error (only detected when using webbrick and looking at stderr)
+        # on some emails that have oddities.  % seems to be the worst offender but there are others.
+        # message = message.encode('UTF-8', :invalid => :replace, :undef => :replace)
+        # won't do it -- we need to url-encode the POST params.  We don't try setting the Content Type and let rack figure it out instead
+        # (probably the default is url_encoded anyway).
+        # Trying to set request.headers['Content-Length'] to the size of the message doesn't seem needed and may crash the server.
+        # Trying to convince Rack to do the right thing with unencoded params to the POST doesn't seem to work either:
+        # (e.g., setting request.headers['Content-Type'] to 'text/plain' or 'message/rfc822').
+
         connection.post do |request|
           request.url @mailbox.delivery_url
-          request.body = message
-          # request.options[:timeout] = 3
-          # request.headers['Content-Type'] = 'text/plain'
+          request.body = URI.encode(message)
         end
+
       end
     end
   end
